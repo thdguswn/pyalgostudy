532957dd8136ed15899fdeb0189de8ad329f8632
Hyunju Song <sarah.hyunjusong@gmail.com>
2021-01-16 23:16:00 +0900

    1/16/2021

---
+++ 10818.py
@@ -0,0 +1,5 @@
+N=int(input())
+num_list =list(map(int, input().split()))
+print(min(num_list),max(num_list))
+
+
--- 1110.py
+++ 1110.py
@@ -3,11 +3,12 @@ if(num <10):
     num = '0'+str(num)
 else:
     num=str(num)
-temp = str(num[1]) + str(((int(num[0]) + int(num[1]))) % 10)
+temp = num[1] + str(((int(num[0]) + int(num[1]))) % 10)
 sol=1
 while(1):
     if(num==temp):
         break
-    temp = str(temp[1]) + str(((int(temp[0]) + int(temp[1]))) % 10)
-    sol+=1
+    else:
+        temp = str(temp[1]) + str(((int(temp[0]) + int(temp[1]))) % 10)
+        sol+=1
 print(sol)
