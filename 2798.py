cddc1ea1734a386c94eafb3343cb378dfbef4ccc
Hyunju Song <sarah.hyunjusong@gmail.com>
2021-01-18 23:53:20 +0900

    1/18/2021

---
+++ 2798.py
@@ -0,0 +1,6 @@
+a,b=map(int,input().split())
+def add_number(num1,num2,num3):
+    return num1+num2+num3
+
+    num_list=list(map(int,input().split())
+        print(return)
---
+++ def function practice.py
@@ -0,0 +1,6 @@
+def add_many(*args):
+    result=0
+    for i in args:
+        result=result+i
+    return result
+#input add_many(1,2,3,4,5)
